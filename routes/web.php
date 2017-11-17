1e0f953b1f2c2f4ae0c33453c79cfb014c246ba8
Paska Anugrah Adil <paskaadil@gmail.com>
2017-11-17 23:11:32 +0700

    project ta web

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::prefix('user')->group(function() {
+	Route::get('/', function () {
+		return view('auth/user');
+	});
+	Route::get('/login', function () {
+		return view('auth/login');
+	});
+	Route::get('/register', function () {
+		return view('auth/register');
+	});
+	
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
